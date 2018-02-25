ab14ae9405daafad137c8b45f6efb31d59bbf9be
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-02-25 21:00:08 +0100

    Better inspection of cursor

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ fn main() {
     let client = Client::connect("localhost", 27017)
         .expect("Failed to initialize standalone client.");
 
-    let coll = client.db("documents").collection("documents");
+    let coll: mongodb::coll::Collection = client.db("documents").collection("documents");
 
-    let doc = doc!{
+    let doc: bson::Document = doc!{
         "title"  => "Posta: proprietà al 31/12/2017",
         "binder" => "Documenti",
         "folder" => "ISEE 2017",
@@ -20,7 +20,8 @@ fn main() {
         "tags"   => ["ISEE", "Posta"]
     };
 
-    coll.delete_many(doc!{"binder" => "Altri"}, None).unwrap();
+    // remove all entries (empty matches all)
+    coll.delete_many(doc!{}, None).unwrap();
 
     // Insert document into 'test.movies' collection
     coll.insert_one(doc.clone(), None)
@@ -28,17 +29,12 @@ fn main() {
         .expect("Failed to insert document.");
 
     // Find the document and receive a cursor
-    let cursor = coll.find(Some(doc!{}), None)
+    let cursor = coll.find(None, None)
         .ok().expect("Failed to execute find.");
 
     for item in cursor {
-        // cursor.next() returns an Option<Result<Document>>
-        match item {
-            Ok(doc) => match doc.get("binder") {
-                Some(&Bson::String(ref title)) => println!("{:?}", title),
-                _ => println!("whoops"),
-            },
-            Err(_) => panic!("Failed to get next from server!")
+        for (key, value) in item.unwrap() {
+            println!("{:?} -> {:?}", key, value);
         }
     }
 }
