bf0922893efcf9735d8e33d65237a1d516c8a313
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-03-03 21:45:02 +0100

    Added web framework Rocket integration

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 #![allow(unused_imports)]
+#![feature(plugin, decl_macro)]
+#![plugin(rocket_codegen)]
+
 #[macro_use]
 extern crate derive_builder;
 
@@ -9,6 +12,7 @@ extern crate lazy_static;
 extern crate bson;
 extern crate mongodb;
 extern crate regex;
+extern crate rocket;
 
 #[macro_use]
 extern crate serde_derive;
@@ -19,30 +23,49 @@ use bson::Bson;
 use mongodb::{Client, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
 use regex::Regex;
+use rocket::response::NamedFile;
 use std::fmt;
+use std::io;
+use std::path::{Path, PathBuf};
 
 mod model;
 mod dbutils;
 use model::{File, Format, Document, DocumentBuilder};
 use dbutils::Adapter;
 
-fn main() {
-    let file = File::new("ciao)$%pollo", Format::new("pdf").unwrap()).unwrap();
-
-    let my_doc = DocumentBuilder::default()
-        .title("c")
-        .binder("rosso")
-        .folder("quella")
-        .year(2010)
-        .file(file)
-        .tags(vec!["uno".into(), "due".into()])
-        .build()
-        .unwrap();
-
-    let adapt = Adapter::new("prova").unwrap();
-    adapt.add(my_doc.clone()).unwrap();
-
-    for doc in &adapt.get(Some(doc!["title" => "c"])).unwrap() {
-        println!("doc: {:?}", doc);
+#[get("/all_docs")]
+fn all_docs() -> String {
+    let adapter = match Adapter::new("documents") {
+        Ok(adapt) => adapt,
+        Err(msg) => panic!(msg)
+    };
+    let documents = match adapter.get_all() {
+        Ok(docs) => docs,
+        Err(_) => panic!("Unable to collect documents")
+    };
+    match serde_json::to_string(&documents) {
+        Ok(string) => string,
+        Err(_) => panic!("Unable to jsonify vector of documents")
     }
+}
+
+#[get("/formats")]
+fn formats() -> String {
+    match serde_json::to_string(&Format::variants()) {
+        Ok(string) => string,
+        Err(_) => panic!("Unable to jsonify vector of documents")
+    }
+}
+
+#[get("/documents/<file..>")]
+fn documents(file: PathBuf) -> Result<NamedFile, io::Error> {
+    NamedFile::open(Path::new("documents/").join(file))
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![all_docs,
+                            formats,
+                            documents])
+        .launch();
 }
