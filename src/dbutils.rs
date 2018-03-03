0f2928a32e9871c39bf495f79a5b43ba8f43bcdf
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-03-03 20:58:08 +0100

    Splitted project into modules

---
+++ src/dbutils.rs
@@ -0,0 +1,49 @@
+use bson;
+use mongodb::coll::Collection;
+use mongodb::{Client, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
+use regex::Regex;
+use std::fmt;
+
+use model::{File, Format, Document, DocumentBuilder};
+
+pub struct Adapter(Collection);
+
+impl<'a> Adapter {
+    pub fn new(name: &'a str) -> Result<Adapter, String> {
+        match Client::connect("localhost", 27017) {
+            Ok(client) => {
+                let coll = client.db("db").collection(name);
+                Ok(Adapter(coll))
+            },
+            Err(_) => Err(format!("Unable to connect to database localhost:27017"))
+        }
+    }
+
+    pub fn add(&self, document: Document) -> Result<(), String> {
+        match self.0.insert_one(document.into(), None) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(format!("Unable to insert into database"))
+        }
+    }
+
+    pub fn get_all(&self) -> Result<Vec<Document>, String> {
+        self.get(None)
+    }
+
+    pub fn get(&self, doc: Option<bson::Document>) -> Result<Vec<Document>, String> {
+        match self.0.find(doc, None).ok() {
+            Some(cursor) => {
+                let mut docs: Vec<Document> = vec![];
+                for item in cursor {
+                    match item {
+                        Ok(doc) => docs.push(doc.into()),
+                        Err(_) => return Err(format!("Error in cursor iteration"))
+                    }
+                }
+                Ok(docs)
+            },
+            None => Err(format!("Unable to fetch from database"))
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 #![allow(unused_imports)]
-use std::fmt;
-
 #[macro_use]
 extern crate derive_builder;
 
@@ -21,150 +19,12 @@ use bson::Bson;
 use mongodb::{Client, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
 use regex::Regex;
+use std::fmt;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-enum Format {
-    PDF,
-    DOC,
-    DOCX,
-    JPEG,
-    TXT,
-    ODG,
-    ODT,
-}
-
-impl Default for Format {
-    fn default() -> Format {
-        Format::TXT
-    }
-}
-
-impl<'a> Format {
-    fn new(name: &'a str) -> Result<Format, String> {
-        match name {
-            "PDF"  | "pdf"  => Ok(Format::PDF),
-            "DOC"  | "doc"  => Ok(Format::DOC),
-            "DOCX" | "docx" => Ok(Format::DOCX),
-            "JPEG" | "jpeg" => Ok(Format::JPEG),
-            "TXT"  | "txt"  => Ok(Format::TXT),
-            "ODG"  | "odg"  => Ok(Format::ODG),
-            "ODT"  | "odt"  => Ok(Format::ODT),
-            _ => Err(format!("Unsupported format: {}", name)),
-        }
-    }
-}
-
-impl fmt::Display for Format {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let name = format!("{:?}", self);
-        write!(f, "{}", name.to_lowercase())
-    }
-}
-
-#[derive(Default, Debug, Serialize, Deserialize, Clone)]
-struct File {
-    path: String,
-    format: Format,
-}
-
-impl<'a> File {
-    fn new(path: &'a str, format: Format) -> Result<File, String> {
-        lazy_static! {
-            static ref RE: Regex = Regex::new("\\W+").unwrap();
-        }
-        let fixed_name: String = RE.replace_all(path, "_").into();
-
-        match fixed_name.as_ref() {
-            "" => Err(format!("Invalid file name '{}'", path)),
-            _ => Ok(File {
-                path: fixed_name,
-                format: format,
-            }),
-        }
-    }
-}
-
-impl fmt::Display for File {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}.{}", self.path, self.format)
-    }
-}
-
-#[derive(Default, Debug, Serialize, Deserialize, Builder, Clone)]
-#[builder(setter(into))]
-struct Document {
-    title: String,
-    binder: String,
-    folder: String,
-    year: i32,
-    file: File,
-    tags: Vec<String>,
-}
-
-impl Into<bson::Document> for Document {
-    fn into(self) -> bson::Document {
-        // I decided to panic here, since conversion should always work
-        let bson_self: bson::Bson = match bson::to_bson(&self) {
-            Ok(bson) => bson,
-            Err(_)   => panic!("Error in Document -> bson::Bson")
-        };
-        match bson_self {
-            bson::Bson::Document(ordered_doc) => ordered_doc,
-            _ => panic!("Invalid bson::Bson enum: not a bson::Document")
-        }
-    }
-}
-
-impl From<bson::Document> for Document {
-    fn from(item: bson::Document) -> Self {
-        let bson_item: bson::Bson = bson::Bson::Document(item);
-        match bson::from_bson(bson_item) {
-            Ok(doc) => doc,
-            Err(_) => panic!("Error in generating document from bson")
-        }
-    }
-}
-
-struct Adapter(mongodb::coll::Collection);
-
-impl<'a> Adapter {
-    fn new(name: &'a str) -> Result<Adapter, String> {
-        match Client::connect("localhost", 27017) {
-            Ok(client) => {
-                let coll = client.db("db").collection(name);
-                Ok(Adapter(coll))
-            },
-            Err(_) => Err(format!("Unable to connect to database localhost:27017"))
-        }
-    }
-
-    fn add(&self, document: Document) -> Result<(), String> {
-        match self.0.insert_one(document.into(), None) {
-            Ok(_) => Ok(()),
-            Err(_) => Err(format!("Unable to insert into database"))
-        }
-    }
-
-    fn get_all(&self) -> Result<Vec<Document>, String> {
-        self.get(None)
-    }
-
-    fn get(&self, doc: Option<bson::Document>) -> Result<Vec<Document>, String> {
-        match self.0.find(doc, None).ok() {
-            Some(cursor) => {
-                let mut docs: Vec<Document> = vec![];
-                for item in cursor {
-                    match item {
-                        Ok(doc) => docs.push(doc.into()),
-                        Err(_) => return Err(format!("Error in cursor iteration"))
-                    }
-                }
-                Ok(docs)
-            },
-            None => Err(format!("Unable to fetch from database"))
-        }
-    }
-}
+mod model;
+mod dbutils;
+use model::{File, Format, Document, DocumentBuilder};
+use dbutils::Adapter;
 
 fn main() {
     let file = File::new("ciao)$%pollo", Format::new("pdf").unwrap()).unwrap();
