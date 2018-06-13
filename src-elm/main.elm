b2abb5171be214ae69ae329c414c7d1931278c0f
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-06-13 10:50:11 +0200

    Working on elm: continue or not?

--- src-elm/main.elm
+++ src-elm/main.elm
@@ -1,4 +1,38 @@
 import Html exposing (text)
+-- import Json.Decode exposing (..)
+import Json.Encode exposing (..)
+
+type Format = PDF | DOC | DOCX | JPEG | TXT | ODG | ODT
+
+encodeFormat : Format -> Value
+encodeFormat format =
+    object
+    [("format",
+         case format of
+         PDF -> string "PDF"
+         DOC -> string "DOC"
+         DOCX -> string "DOCX"
+         JPEG -> string "JPEG"
+         TXT -> string "TXT"
+         ODG -> string "ODG"
+         ODT -> string "ODT")
+    ]
+
+type alias File =
+  { path : String
+  , format : Format
+  }
+
+type alias Document =
+    { title: String
+    , binder: String
+    , folder: String
+    , year: Int
+    , file: File
+    , tags: List String
+    }
+
+-- a = Document "ciao" "pollo" "asd" 10 (File "ciao" PDF) ["ciao", "asd"]
 
 main =
-  text "Hello, World!"
+  text (encode 2 (encodeFormat PDF))
